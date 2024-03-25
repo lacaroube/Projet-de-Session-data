ebb1982659d8ff323bbbe17ffde72e6bdb1afe9a
Cédric-B. Marsolais <cedric-b.marsolais.1@ulaval.ca>
2024-03-25 14:24:18 -0400

    init- project structure

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+
+from database import insert_avis
 
 app = Flask(__name__)
 
@@ -6,6 +8,16 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/avis", methods=["POST"])
+def add_avis():
+    data = request.get_json
+    insert_avis(data["text"])
+    response = {
+        "status": "success"
+    }
+    return jsonify(response)
 
 
 if __name__ == '__main__':
