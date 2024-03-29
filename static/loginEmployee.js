a752b1d2eed4c308dedd924d647fcf18d2a92356
Raphaël <rachh1@ulaval.ca>
2024-03-29 12:31:39 -0400

    Feat: Début page admin
    On peut désormais se connecter en tant qu'administrateur (par la base de données utilisateurs)

---
+++ static/loginEmployee.js
@@ -0,0 +1,33 @@
+function loginAsEmployee() {
+    window.location.replace("/")
+}
+
+async function loginAsAdmin() {
+    const usernameInput = document.getElementById("username")
+    const username = usernameInput.value
+    const passwordInput = document.getElementById("password")
+    const password = passwordInput.value
+
+    const data = await getAdmin(username, password)
+    if (data.status === "success") {
+        sessionStorage.setItem('username', data.client[1]);
+        sessionStorage.setItem('id', data.client[0]);
+        window.location.href = "utilisateur.html";
+    }
+}
+
+function getAdmin(username, password) {
+    const getUrl = "get-client"
+    return fetch(getUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    }).then(function (response) {
+        return response.json()
+    })
+}
